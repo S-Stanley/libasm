a69dbf94dbe50d1ae61e198f6a18f00a0045b0d6
Stanley SERBIN <stanleyserbin@gmail.com>
2023-02-17 19:48:19 +0000

    document strlen

--- srcs/ft_strlen.asm
+++ srcs/ft_strlen.asm
@@ -3,14 +3,14 @@ BITS 64
 global ft_strlen
 
 ft_strlen:
-	mov rax, 0
-	jmp loop
+	mov rax, 0			; init iterator to 0
+	jmp L1				; start loop
 
-loop:
-	cmp BYTE [rdi + rax], 0
-	je exit
-	inc rax
-	jmp loop
+L1:
+	cmp BYTE [rdi + rax], 0		; check if string is finish
+	je L2				; end the loop
+	inc rax				; increment iterator
+	jmp L1				; continue the loop
 
-exit:
-	ret
+L2:
+	ret				; return iterator (rax)
